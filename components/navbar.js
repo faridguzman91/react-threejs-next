5ae66db6fb3f4f28bd00b4a3903f9f693dc90476
Jimmy Guzman <jimmy@creapolis.com>
2021-12-18 22:51:44 +0100

    navbar

--- components/navbar.js
+++ components/navbar.js
@@ -19,4 +19,33 @@ import { HamburgerIcon } from '@chakra-ui/icons'
 const LinkItem = ({ href, path, children }) => {
     const active = path === href
     const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+
+    return (
+        <NextLink href={href}>
+            <Link p={2}
+            bg={active ? 'glassTeal' : undefined }
+            color={active ? '#202023' : inactiveColor}
+            >
+            {children}
+            </Link>
+        </NextLink>
+    )
+}
+
+const Navbar = props => {
+    const {path} = props
+
+    return (
+        <Box position="fixed"
+        as="nav"
+        w="100%"
+        bg={useColorModeValue('#ffffff40', '#20202380')}
+        style={{backdropFilter: 'blur(10px)'}}
+        zIndex={1}
+        {...props}
+        >
+            Navbar
+        </Box>
+
+    )
 }
